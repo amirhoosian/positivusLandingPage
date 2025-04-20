1ffec2fd309c3bd1352307252d4aaf3f94bc1bab
amirhoosian <amirhoosian78@gmail.com>
2025-04-20 19:50:26 +0330

    add s;ider

---
+++ src/index.js
@@ -0,0 +1,7 @@
+  import Swiper from 'swiper';
+  import 'swiper/swiper-bundle.css';
+  const swiper = new Swiper('.swiper-container', {
+    spaceBetween: 50,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+  });
